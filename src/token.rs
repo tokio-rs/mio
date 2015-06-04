608776f3dcf0a708216b257b94865b9ba2641e0e
Dawid Ciężarkiewicz <dpc@dpc.pw>
2015-06-03 19:47:02 -0700

    Abstract Token away in Slab module.
    This should make it possible to move it to separate crate as it might be useful
    on it's own.

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use util::Index;
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub struct Token(pub usize);
 
@@ -6,5 +8,15 @@ impl Token {
     pub fn as_usize(self) -> usize {
         let Token(inner) = self;
         inner
+    }
+}
+
+impl Index for Token {
+    fn from_usize(i : usize) -> Token {
+        Token(i)
+    }
+
+    fn as_usize(&self) -> usize {
+        Token::as_usize(*self)
     }
 }
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,7 +1,10 @@
 //! Utilities for non-blocking IO programs
 
 pub use self::mpmc_bounded_queue::Queue as BoundedQueue;
-pub use self::slab::Slab;
+pub use self::slab::Index;
+
+use token::Token;
+pub type Slab<T> = slab::Slab<T, Token>;
 
 mod mpmc_bounded_queue;
 mod slab;
