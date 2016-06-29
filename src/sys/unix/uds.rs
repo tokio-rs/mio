0188220db36adfb5bf085d6cda7cc93207713e00
Frank Rehberger <frehberg@gmail.com>
2016-06-29 22:18:22 +0200

    adapt to latest nix (MsgFlags::empty())

--- src/sys/unix/uds.rs
+++ src/sys/unix/uds.rs
@@ -4,7 +4,7 @@ use sys::unix::{net, nix, Socket};
 use std::io::{Read, Write};
 use std::path::Path;
 use std::os::unix::io::{RawFd, IntoRawFd, AsRawFd, FromRawFd};
-pub use nix::sys::socket::Shutdown;
+pub use nix::sys::socket::{Shutdown, MsgFlags};
 
 #[derive(Debug)]
 pub struct UnixSocket {
@@ -57,7 +57,7 @@ impl UnixSocket {
     pub fn read_recv_fd(&mut self, buf: &mut [u8]) -> io::Result<(usize, Option<RawFd>)> {
         let iov = [nix::IoVec::from_mut_slice(buf)];
         let mut cmsgspace: nix::CmsgSpace<[RawFd; 1]> = nix::CmsgSpace::new();
-        let msg = try!(nix::recvmsg(self.io.as_raw_fd(), &iov, Some(&mut cmsgspace), 0)
+        let msg = try!(nix::recvmsg(self.io.as_raw_fd(), &iov, Some(&mut cmsgspace), MsgFlags::empty())
                            .map_err(super::from_nix_error));
         let mut fd = None;
         for cmsg in msg.cmsgs() {
@@ -76,7 +76,7 @@ impl UnixSocket {
         let iov = [nix::IoVec::from_slice(buf)];
         let fds = [fd];
         let cmsg = nix::ControlMessage::ScmRights(&fds);
-        nix::sendmsg(self.io.as_raw_fd(),&iov, &[cmsg], 0, None)
+        nix::sendmsg(self.io.as_raw_fd(),&iov, &[cmsg], MsgFlags::empty(), None)
             .map_err(super::from_nix_error)
     }
 }
