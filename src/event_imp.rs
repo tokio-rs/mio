b881175126c62adfb7604dfa2e9d27847160798c
Carl Lerche <me@carllerche.com>
2017-02-21 15:31:56 -0800

    Add documentation for Evented

--- src/event_imp.rs
+++ src/event_imp.rs
@@ -2,11 +2,175 @@ use {Poll, Token};
 use std::{fmt, io, ops};
 
 /// A value that may be registered with `Poll`
+///
+/// Values that implement `Evented` can be registered with `Poll`. Users of Mio
+/// should not use the `Evented` trait functions directly. Instead, the
+/// equivalent functions on `Poll` should be used.
+///
+/// See [`Poll`] for more details.
+///
+/// # Implementing `Evented`
+///
+/// There are two types of `Evented` values.
+///
+/// * **System** handles, which are backed by sockets or other system handles.
+/// These `Evented` handles will be monitored by the system selector. In this
+/// case, an implementation of `Evented` delegates to a lower level handle.
+///
+/// * **User** handles, which are driven entirely in user space using
+/// [`Registration`] and [`SetReadiness`]. In this case, the implementer takes
+/// responsibility for driving the readiness state changes.
+///
+/// # Examples
+///
+/// Implementing `Evented` on a struct containing a socket:
+///
+/// ```
+/// use mio::{Ready, Poll, PollOpt, Token};
+/// use mio::event::Evented;
+/// use mio::tcp::TcpStream;
+///
+/// use std::io;
+///
+/// pub struct MyEvented {
+///     socket: TcpStream,
+/// }
+///
+/// impl Evented for MyEvented {
+///     fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
+///         -> io::Result<()>
+///     {
+///         // Delegate the `register` call to `socket`
+///         self.socket.register(poll, token, interest, opts)
+///     }
+///
+///     fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
+///         -> io::Result<()>
+///     {
+///         // Delegate the `reregister` call to `socket`
+///         self.socket.reregister(poll, token, interest, opts)
+///     }
+///
+///     fn deregister(&self, poll: &Poll) -> io::Result<()> {
+///         // Delegate the `deregister` call to `socket`
+///         self.socket.deregister(poll)
+///     }
+/// }
+/// ```
+///
+/// Implement `Evented` using [`Registration`] and [`SetReadiness`].
+///
+/// ```
+/// use mio::{Ready, Registration, Poll, PollOpt, Token};
+/// use mio::event::Evented;
+///
+/// use std::io;
+/// use std::sync::Mutex;
+/// use std::time::Instant;
+/// use std::thread;
+///
+/// pub struct Deadline {
+///     when: Instant,
+///     registration: Mutex<Option<Registration>>,
+/// }
+///
+/// impl Deadline {
+///     pub fn is_elapsed(&self) -> bool {
+///         Instant::now() >= self.when
+///     }
+/// }
+///
+/// impl Evented for Deadline {
+///     fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
+///         -> io::Result<()>
+///     {
+///         let mut registration = self.registration.lock().unwrap();
+///
+///         if registration.is_some() {
+///             return Err(io::Error::new(io::ErrorKind::Other, "already registered"));
+///         }
+///
+///         let (r, set_readiness) = Registration::new(poll, token, interest, opts);
+///         *registration = Some(r);
+///
+///         let when = self.when;
+///
+///         thread::spawn(move || {
+///             let now = Instant::now();
+///
+///             if now < when {
+///                 thread::sleep(when - now);
+///             }
+///
+///             set_readiness.set_readiness(Ready::readable());
+///         });
+///
+///         Ok(())
+///     }
+///
+///     fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt)
+///         -> io::Result<()>
+///     {
+///         match *self.registration.lock().unwrap() {
+///             Some(ref registration) => registration.update(token, interest, opts),
+///             None => Err(io::Error::new(io::ErrorKind::Other, "not registered")),
+///         }
+///     }
+///
+///     fn deregister(&self, poll: &Poll) -> io::Result<()> {
+///         let _ = self.registration.lock().unwrap().take();
+///     }
+/// }
+/// ```
+///
+/// [`Poll`]: struct.Poll.html
+/// [`Registration`]: struct.Registration.html
+/// [`SetReadiness`]: struct.SetReadiness.html
 pub trait Evented {
+    /// Register `self` with the given `Poll` instance.
+    ///
+    /// This function should not be called directly. Use [`Poll::register`]
+    /// instead.
+    ///
+    /// Implementors should handle registration by either delegating the call to
+    /// another `Evented` type or creating a [`Registration`].
+    ///
+    /// See [struct] documentation for more details.
+    ///
+    /// [`Poll::register`]: struct.Poll.html#method.register
+    /// [`Registration`]: struct.Registration.html
+    /// [struct]: #
     fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()>;
 
+    /// Re-register `self` with the given `Poll` instance.
+    ///
+    /// This function should not be called directly. Use [`Poll::reregister`]
+    /// instead.
+    ///
+    /// Implementors should handle re-registration by either delegating the call to
+    /// another `Evented` type or calling [`Registration::update`].
+    ///
+    /// See [struct] documentation for more details.
+    ///
+    /// [`Poll::reregister`]: struct.Poll.html#method.register
+    /// [`Registration::update`]: struct.Registration.html#method.update
+    /// [struct]: #
     fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> io::Result<()>;
 
+    /// Deregister `self` from the given `Poll` instance
+    ///
+    /// This function should not be called directly. Use [`Poll::deregister`]
+    /// instead.
+    ///
+    /// Implementors shuld handle deregistration by either delegating the call
+    /// to another `Evented` type or by dropping the [`Registration`] associated
+    /// with `self`.
+    ///
+    /// See [struct] documentation for more details.
+    ///
+    /// [`Poll::deregister`]: struct.Poll.html#method.deregister
+    /// [`Registration`]: struct.Registration.html
+    /// [struct]: #
     fn deregister(&self, poll: &Poll) -> io::Result<()>;
 }
 
