453c01e0b16d4e59564652ef324fea4658821a3e
Ahmed Charles <acharles@outlook.com>
2017-08-20 00:00:37 +0000

    Remove Event and Evented at the crate root.

--- src/lib.rs
+++ src/lib.rs
@@ -154,11 +154,6 @@ pub use event::{
     Events,
 };
 
-#[deprecated(since = "0.6.5", note = "use events:: instead")]
-#[cfg(feature = "with-deprecated")]
-#[doc(hidden)]
-pub use event::{Event, Evented};
-
 #[deprecated(since = "0.6.5", note = "use events::Iter instead")]
 #[cfg(feature = "with-deprecated")]
 #[doc(hidden)]
