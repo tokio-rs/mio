ccbfb4b17b689dd0f7bd9e93abc4da7a558e18f0
Justin Mayhew <mayhew@live.ca>
2017-08-23 20:46:48 -0300

    Fix broken links
    Includes a few minor typo fixes and other random improvements.

--- src/token.rs
+++ src/token.rs
@@ -115,7 +115,7 @@
 ///                         Ok(_) => unreachable!(),
 ///                         Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
 ///                             // Socket is not ready anymore, stop reading
-///                             continue;
+///                             break;
 ///                         }
 ///                         e => panic!("err={:?}", e), // Unexpected error
 ///                     }
