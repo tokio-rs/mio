9eac5ffefbf63c61cdb1fb0f121ab2c0c74a315f
Kam Y. Tse <kevin.xjy@gmail.com>
2018-01-05 07:10:07 +0800

    Remove unnecessary constants definition for android (#792)

--- src/sys/unix/epoll.rs
+++ src/sys/unix/epoll.rs
@@ -5,19 +5,9 @@ use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
 use std::time::Duration;
 use std::{cmp, i32};
 
-use libc::c_int;
-use libc;
-use libc::{EPOLLERR, EPOLLHUP};
+use libc::{self, c_int};
+use libc::{EPOLLERR, EPOLLHUP, EPOLLRDHUP, EPOLLONESHOT};
 use libc::{EPOLLET, EPOLLOUT, EPOLLIN, EPOLLPRI};
-
-#[cfg(not(target_os = "android"))]
-use libc::{EPOLLRDHUP, EPOLLONESHOT};
-
-// libc doesn't define these constants on android, but they are supported.
-#[cfg(target_os = "android")]
-const EPOLLRDHUP: libc::c_int = 0x00002000;
-#[cfg(target_os = "android")]
-const EPOLLONESHOT: libc::c_int = 0x40000000;
 
 use {io, Ready, PollOpt, Token};
 use event_imp::Event;
@@ -80,9 +70,9 @@ impl Selector {
         unsafe {
             evts.events.set_len(0);
             let cnt = cvt(libc::epoll_wait(self.epfd,
-                                                evts.events.as_mut_ptr(),
-                                                evts.events.capacity() as i32,
-                                                timeout_ms))?;
+                                           evts.events.as_mut_ptr(),
+                                           evts.events.capacity() as i32,
+                                           timeout_ms))?;
             let cnt = cnt as usize;
             evts.events.set_len(cnt);
 
