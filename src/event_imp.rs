fdcf82064d376facba93c9fed4b042b2e551b960
Marc-André Lureau <marcandre.lureau@redhat.com>
2018-12-11 23:08:22 +0400

    unix: update READY_ALL to take PRI into account
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/event_imp.rs
+++ src/event_imp.rs
@@ -685,7 +685,7 @@ impl Ready {
     /// Returns a `Ready` representing readiness for all operations.
     ///
     /// This includes platform specific operations as well (`hup`, `aio`,
-    /// `error`, `lio`).
+    /// `error`, `lio`, `pri`).
     ///
     /// See [`Poll`] for more documentation on polling.
     ///
