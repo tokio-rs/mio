d75b2e79ae7f317c6baebf77616f70d599a25349
Carl Lerche <me@carllerche.com>
2019-04-28 09:31:44 -0700

    Fix missing tests

--- test/test_double_register.rs
+++ test/test_double_register.rs
@@ -12,9 +12,11 @@ pub fn test_double_register() {
     let l = TcpListener::bind(&"127.0.0.1:0".parse().unwrap()).unwrap();
 
     // Register the listener with `Poll`
-    poll.register(&l, Token(0), Ready::readable(), PollOpt::edge())
+    poll.register()
+        .register(&l, Token(0), Ready::readable(), PollOpt::edge())
         .unwrap();
     assert!(poll
+        .register()
         .register(&l, Token(1), Ready::readable(), PollOpt::edge())
         .is_err());
 }
