e610594fbcd9f57306422088a31e3ac8e3367b9c
Carl Lerche <me@carllerche.com>
2019-05-13 12:18:02 -0700

    Add TCP shutdown test

---
+++ test/test_tcp_shutdown.rs
@@ -0,0 +1,98 @@
+use std::cmp;
+use std::io::prelude::*;
+use std::io;
+use std::net::Shutdown;
+use std::thread;
+use std::time::Duration;
+
+use net2::{self, TcpStreamExt};
+
+use {TryRead, TryWrite};
+use mio::{Token, Ready, PollOpt, Poll, Events};
+use iovec::IoVec;
+use mio::net::{TcpListener, TcpStream};
+
+macro_rules! wait {
+    ($poll:ident, $ready:ident) => {{
+        use std::time::Instant;
+
+        let now = Instant::now();
+        let mut events = Events::with_capacity(16);
+
+        'outer:
+        loop {
+            if now.elapsed() > Duration::from_secs(5) {
+                panic!("not ready");
+            }
+
+            $poll.poll(&mut events, Some(Duration::from_secs(1))).unwrap();
+
+            for event in &events {
+                if event.token() == Token(0) && event.readiness().$ready() {
+                    break 'outer
+                }
+            }
+        }
+    }};
+}
+
+fn setup() -> (Poll, std::net::TcpStream, TcpStream) {
+    let poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(16);
+
+    let listener = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
+    let addr = listener.local_addr().unwrap();
+
+
+
+    let mut client = TcpStream::connect(&addr).unwrap();
+    poll.register(&client,
+                  Token(0),
+                  Ready::writable(),
+                  PollOpt::edge()).unwrap();
+
+    let (socket, _) = listener.accept().unwrap();
+
+    wait!(poll, is_writable);
+
+    // Write until not writable
+    loop {
+        match client.write(b"hello world") {
+            Ok(_) => {}
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => break,
+            Err(e) => panic!("error = {:?}", e),
+        }
+    }
+
+    (poll, socket, client)
+}
+
+#[test]
+fn test_write_shutdown() {
+    let (poll, socket, client) = setup();
+    let mut events = Events::with_capacity(16);
+
+    // Polling should not have any events
+    poll.poll(&mut events, Some(Duration::from_millis(100))).unwrap();
+    assert!(events.iter().next().is_none());
+
+    // Now, shutdown the write half of the socket.
+    socket.shutdown(Shutdown::Write).unwrap();
+
+    wait!(poll, is_readable);
+}
+
+#[test]
+fn test_read_shutdown() {
+    let (poll, socket, client) = setup();
+    let mut events = Events::with_capacity(16);
+
+    // Polling should not have any events
+    poll.poll(&mut events, Some(Duration::from_millis(100))).unwrap();
+    assert!(events.iter().next().is_none());
+
+    // Now, shutdown the write half of the socket.
+    socket.shutdown(Shutdown::Read).unwrap();
+
+    wait!(poll, is_writable);
+}
