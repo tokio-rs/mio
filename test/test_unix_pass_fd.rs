4c523ef45dcf6d37406ee3113f9820211a9e64a5
FXTi <zjxiang1998@gmail.com>
2019-05-14 22:57:29 +0800

    Fix broken test cases

--- test/test_unix_pass_fd.rs
+++ test/test_unix_pass_fd.rs
@@ -82,7 +82,9 @@ impl EchoConn {
             }
             Err(e) => {
                 debug!("not implemented; client err={:?}", e);
-                if let Some(x) = self.interests.as_mut() {
+                if self.interests == Some(Interests::readable()) {
+                    self.interests = None;
+                } else if let Some(x) = self.interests.as_mut() {
                     *x -= Interests::readable();
                 }
             }
