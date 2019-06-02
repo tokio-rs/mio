90ba44135099f877451fac865a0b66c3523cb249
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-06-02 15:31:12 +0200

    Setup a common crate for Mio

---
+++ mio-common/src/lib.rs
@@ -0,0 +1,5 @@
+//! Crate of types shared by Mio and the platform specific crates.
+
+// This crate is needed to prevent cyclic depedencies, where e.g. mio would
+// depend on mio-kqueue for the `Selector` and mio-kqueue would depend on mio
+// for the `Token`.
