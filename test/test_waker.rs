31e271ae3f7f45fc59e5cb9e662e8a82e96357aa
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-06-17 12:39:22 +0200

    Make expect_waker_event test less strict
    Now it accept mutltiple waker events, rather then just one.

--- test/test_waker.rs
+++ test/test_waker.rs
@@ -92,9 +92,9 @@ fn waker_multiple_wakeups_different_thread() {
 
 fn expect_waker_event(poll: &mut Poll, events: &mut Events, token: Token) {
     poll.poll(events, Some(Duration::from_millis(100))).unwrap();
-    let mut events = events.iter();
-    let event = events.next().unwrap();
-    assert_eq!(event.token(), token);
-    assert!(event.is_readable());
-    assert!(events.next().is_none());
+    assert!(!events.is_empty());
+    for event in events.iter() {
+        assert_eq!(event.token(), token);
+        assert!(event.is_readable());
+    }
 }
