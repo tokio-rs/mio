e8d068dc64a695dd2cb00294d83c48519bf837d5
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-06-20 19:58:40 +0200

    Document the requirements for Event and SysEvent in sys

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,3 +1,13 @@
+//! Module with system specific types.
+//!
+//! `SysEvent`: must be a type alias for the system specific event, e.g.
+//!             `kevent` or `epol_event`.
+//! `Event`: **must be** a `transparent` wrapper around `SysEvent`, i.e. the
+//!          type must have `#[repr(transparent)]` with only `SysEvent` as
+//!          field. This is safety requirement, see `Event::from_sys_event_ref`.
+//!          Furthermore on this type a number of methods must be implemented
+//!          that are used by `Event` (in the `event` module).
+
 #[cfg(unix)]
 pub use self::unix::{
     pipe, set_nonblock, Event, EventedFd, Events, Io, Selector, SysEvent, TcpListener, TcpStream,
