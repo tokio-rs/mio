94584d749cde8f48af287465dae3092e7c263185
Alan Somers <asomers@gmail.com>
2019-07-08 17:13:35 -0600

    Prefer pass-by-value over pass-by-reference for public methods
    pass-by-value is faster than pass-by-reference for types that are small
    and Copy.  This is technically a breaking change since these methods are
    public.  However, most crates should compile fine because the only
    method argument that's changing is `&self`, which is provided
    implicitly.  The only way to get into trouble would be something like
    this:
    
    let interests = Interests::new;
    Interests::is_readable(&ready)

--- src/interests.rs
+++ src/interests.rs
@@ -64,22 +64,22 @@ impl Interests {
     pub const LIO: Interests = Interests(unsafe { NonZeroU8::new_unchecked(LIO) });
 
     /// Returns true if the value includes readable readiness.
-    pub fn is_readable(&self) -> bool {
+    pub fn is_readable(self) -> bool {
         (self.0.get() & READABLE) != 0
     }
 
     /// Returns true if the value includes writable readiness.
-    pub fn is_writable(&self) -> bool {
+    pub fn is_writable(self) -> bool {
         (self.0.get() & WRITABLE) != 0
     }
 
     /// Returns true if `Interests` contains AIO readiness
-    pub fn is_aio(&self) -> bool {
+    pub fn is_aio(self) -> bool {
         (self.0.get() & AIO) != 0
     }
 
     /// Returns true if `Interests` contains LIO readiness
-    pub fn is_lio(&self) -> bool {
+    pub fn is_lio(self) -> bool {
         (self.0.get() & LIO) != 0
     }
 
