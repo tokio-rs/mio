3ca01f4123e02812e4452c6485e5b010e6cec82e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-08-26 15:34:22 +0200

    Document require types in sys module
    Hopefully this will be useful when porting Mio to another OS or selector
    (io_uring anyone?).

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,9 +1,17 @@
 //! Module with system specific types.
 //!
-//! `Event`: a type alias for the system specific event, e.g.
-//!          `kevent` or `epoll_event`.
-//! `event`: a module with various helper functions for `Event`, see
-//!          `crate::event::Event` for the required functions.
+//! Required types:
+//!
+//! * `Event`: a type alias for the system specific event, e.g. `kevent` or
+//!            `epoll_event`.
+//! * `event`: a module with various helper functions for `Event`, see
+//!            [`crate::event::Event`] for the required functions.
+//! * `Events`: collection of `Event`s, see [`crate::Events`].
+//! * `Selector`: selector used to register event sources and poll for events,
+//!               see [`crate::Poll`] and [`crate::Registry`] for required
+//!               methods.
+//! * `TcpListener`, `TcpStream` and `UdpSocket`: see [`crate::net`] module.
+//! * `Waker`: see [`crate::Waker`].
 
 #[cfg(unix)]
 pub use self::unix::{
