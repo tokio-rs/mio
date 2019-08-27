29fd58deda0ea348b6302b706118675ef94fcbda
Carl Lerche <me@carllerche.com>
2019-08-27 10:37:23 -0700

    Fix size test

--- tests/size.rs
+++ tests/size.rs
@@ -1,13 +1,14 @@
+use mio::net::*;
+
 #[test]
+#[cfg(unix)]
 #[cfg(not(debug_assertions))]
 fn assert_size() {
     use std::mem::size_of;
 
-    use crate::sys;
-
     // Without debug assertions enabled `TcpListener`, `TcpStream` and `UdpSocket` should have the
     // same size as the system specific socket, i.e. just a file descriptor on Unix platforms.
-    assert_eq!(size_of::<TcpListener>(), size_of::<sys::TcpListener>());
-    assert_eq!(size_of::<TcpStream>(), size_of::<sys::TcpStream>());
-    assert_eq!(size_of::<UdpSocket>(), size_of::<sys::UdpSocket>());
+    assert_eq!(size_of::<TcpListener>(), size_of::<std::net::TcpListener>());
+    assert_eq!(size_of::<TcpStream>(), size_of::<std::net::TcpStream>());
+    assert_eq!(size_of::<UdpSocket>(), size_of::<std::net::UdpSocket>());
 }
