a08ca2b8c29550980ec969a535845ca1a7d05c1a
Kevin Leimkuhler <kleimkuhler@icloud.com>
2019-09-30 16:38:56 -0700

    Add more tests
    Signed-off-by: Kevin Leimkuhler <kleimkuhler@icloud.com>

--- tests/uds.rs
+++ tests/uds.rs
@@ -4,14 +4,16 @@ mod util;
 use mio::net::{UnixListener, UnixStream};
 use mio::{Interests, Token};
 use std::io::{self, IoSlice, IoSliceMut, Read, Write};
+use std::net::Shutdown;
 #[cfg(unix)]
 use std::os::unix::net::SocketAddr;
 use std::sync::mpsc::{channel, Receiver};
+use std::sync::{Arc, Barrier};
 use std::thread;
 use tempdir::TempDir;
 use util::{
-    assert_send, assert_sync, assert_would_block, expect_events, init_with_poll, ExpectEvent,
-    TryRead, TryWrite,
+    assert_send, assert_sync, assert_would_block, expect_events, expect_no_events, init_with_poll,
+    ExpectEvent, TryRead, TryWrite,
 };
 
 const DATA1: &[u8] = b"Hello world!";
@@ -25,86 +27,7 @@ const REMOTE: Token = Token(1);
 const LOCAL_CLONE: Token = Token(2);
 
 #[test]
-fn is_send_and_sync() {
-    assert_send::<UnixListener>();
-    assert_sync::<UnixListener>();
-
-    assert_send::<UnixStream>();
-    assert_sync::<UnixStream>();
-}
-
-#[test]
-fn accept() {
-    let (mut poll, mut events) = init_with_poll();
-
-    let dir = assert_ok!(TempDir::new("uds"));
-    let path = dir.path().join("path");
-
-    let remote = assert_ok!(UnixListener::bind(path));
-    let addr = assert_ok!(remote.local_addr());
-    let bp = addr.as_pathname().expect("not a pathname");
-
-    let bound_path = bp.to_owned();
-    let handle = thread::spawn(move || {
-        assert_ok!(UnixStream::connect(bound_path));
-    });
-
-    assert_ok!(poll
-        .registry()
-        .register(&remote, REMOTE, Interests::READABLE));
-    expect_events(
-        &mut poll,
-        &mut events,
-        vec![ExpectEvent::new(REMOTE, Interests::READABLE)],
-    );
-
-    assert_ok!(remote.accept());
-    assert_would_block(remote.accept());
-    assert_ok!(handle.join());
-}
-
-#[test]
-fn connect() {
-    let (mut poll, mut events) = init_with_poll();
-
-    let dir = assert_ok!(TempDir::new("uds"));
-    let path = dir.path().join("foo");
-
-    let remote = assert_ok!(UnixListener::bind(path.clone()));
-    let local = assert_ok!(UnixStream::connect(path));
-
-    let (sender_1, receiver_1) = channel();
-    let (sender_2, receiver_2) = channel();
-
-    let handle = thread::spawn(move || {
-        let (stream, _) = assert_ok!(remote.accept());
-        assert_ok!(receiver_1.recv());
-        drop(stream);
-        assert_ok!(sender_2.send(()));
-    });
-
-    assert_ok!(poll
-        .registry()
-        .register(&local, LOCAL, Interests::READABLE | Interests::WRITABLE));
-    expect_events(
-        &mut poll,
-        &mut events,
-        vec![ExpectEvent::new(LOCAL, Interests::WRITABLE)],
-    );
-
-    assert_ok!(sender_1.send(()));
-    assert_ok!(receiver_2.recv());
-
-    expect_events(
-        &mut poll,
-        &mut events,
-        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
-    );
-    assert_ok!(handle.join());
-}
-
-#[test]
-fn stream() {
+fn smoke_test() {
     let (mut poll, mut events) = init_with_poll();
 
     let (sync_sender, sync_receiver) = channel();
@@ -180,6 +103,123 @@ fn stream() {
 }
 
 #[test]
+fn is_send_and_sync() {
+    assert_send::<UnixListener>();
+    assert_sync::<UnixListener>();
+
+    assert_send::<UnixStream>();
+    assert_sync::<UnixStream>();
+}
+
+#[test]
+fn register() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(&local, LOCAL, Interests::READABLE));
+
+    expect_no_events(&mut poll, &mut events);
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
+#[test]
+fn reregister() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(&local, LOCAL, Interests::READABLE));
+    assert_ok!(poll
+        .registry()
+        .reregister(&local, LOCAL_CLONE, Interests::WRITABLE));
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL_CLONE, Interests::WRITABLE)],
+    );
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
+#[test]
+fn deregister() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(&local, LOCAL, Interests::WRITABLE));
+    assert_ok!(poll.registry().deregister(&local));
+
+    expect_no_events(&mut poll, &mut events);
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
+#[test]
+fn connect() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let dir = assert_ok!(TempDir::new("uds"));
+    let path = dir.path().join("foo");
+
+    let remote = assert_ok!(UnixListener::bind(path.clone()));
+    let local = assert_ok!(UnixStream::connect(path));
+
+    let (sender_1, receiver_1) = channel();
+    let (sender_2, receiver_2) = channel();
+
+    let handle = thread::spawn(move || {
+        let (stream, _) = assert_ok!(remote.accept());
+        assert_ok!(receiver_1.recv());
+        drop(stream);
+        assert_ok!(sender_2.send(()));
+    });
+
+    assert_ok!(poll
+        .registry()
+        .register(&local, LOCAL, Interests::READABLE | Interests::WRITABLE));
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::WRITABLE)],
+    );
+
+    assert_ok!(sender_1.send(()));
+    assert_ok!(receiver_2.recv());
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
+    );
+    assert_ok!(handle.join());
+}
+#[test]
 fn try_clone() {
     let (mut poll, mut events) = init_with_poll();
 
@@ -239,6 +279,177 @@ fn try_clone() {
     handle.join().expect("unable to join thread");
 }
 
+#[test]
+fn shutdown_read() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let mut local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(
+        &local,
+        LOCAL,
+        Interests::WRITABLE.add(Interests::READABLE)
+    ));
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::WRITABLE)],
+    );
+
+    let wrote = assert_ok!(local.write(DATA1));
+    assert_eq!(wrote, DATA1_LEN);
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
+    );
+
+    local.shutdown(Shutdown::Read).unwrap();
+
+    // Shutting down the reading side is different on each platform. For example
+    // on Linux based systems we can still read.
+    #[cfg(any(
+        target_os = "dragonfly",
+        target_os = "freebsd",
+        target_os = "ios",
+        target_os = "macos",
+        target_os = "netbsd",
+        target_os = "openbsd"
+    ))]
+    {
+        let mut buf = [0; 64];
+        let read = assert_ok!(local.read(&mut buf));
+        assert_eq!(read, 0);
+    }
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
+#[test]
+fn shutdown_write() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let mut local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(
+        &local,
+        LOCAL,
+        Interests::WRITABLE.add(Interests::READABLE)
+    ));
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::WRITABLE)],
+    );
+
+    let wrote = assert_ok!(local.write(DATA1));
+    assert_eq!(wrote, DATA1_LEN);
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
+    );
+
+    assert_ok!(local.shutdown(Shutdown::Write));
+
+    let err = assert_err!(local.write(DATA2));
+    assert_eq!(err.kind(), io::ErrorKind::BrokenPipe);
+
+    // FIXME: we don't always receive the following event.
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
+    );
+
+    // Read should be ok.
+    let mut buf = [0; 64];
+    let read = assert_ok!(local.read(&mut buf));
+    assert_eq!(read, DATA1_LEN);
+    assert_eq!(&buf[..read], DATA1);
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
+#[test]
+fn shutdown_both() {
+    let (mut poll, mut events) = init_with_poll();
+
+    let (sync_sender, sync_receiver) = channel();
+    let (handle, remote_addr) = echo_remote(1, sync_receiver);
+
+    let path = remote_addr.as_pathname().expect("not a pathname");
+    let mut local = assert_ok!(UnixStream::connect(path));
+    assert_ok!(sync_sender.send(()));
+
+    assert_ok!(poll.registry().register(
+        &local,
+        LOCAL,
+        Interests::WRITABLE.add(Interests::READABLE)
+    ));
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::WRITABLE)],
+    );
+
+    let wrote = assert_ok!(local.write(DATA1));
+    assert_eq!(wrote, DATA1_LEN);
+
+    expect_events(
+        &mut poll,
+        &mut events,
+        vec![ExpectEvent::new(LOCAL, Interests::READABLE)],
+    );
+
+    assert_ok!(local.shutdown(Shutdown::Both));
+
+    // Shutting down the reading side is different on each platform. For example
+    // on Linux based systems we can still read.
+    #[cfg(any(
+        target_os = "dragonfly",
+        target_os = "freebsd",
+        target_os = "ios",
+        target_os = "macos",
+        target_os = "netbsd",
+        target_os = "openbsd"
+    ))]
+    {
+        let mut buf = [0; 64];
+        let read = assert_ok!(local.read(&mut buf));
+        assert_eq!(read, 0);
+    }
+
+    let err = assert_err!(local.write(DATA2));
+    #[cfg(unix)]
+    assert_eq!(err.kind(), io::ErrorKind::BrokenPipe);
+    #[cfg(window)]
+    assert_eq!(err.kind(), io::ErrorKind::ConnectionAbroted);
+
+    // Close the connection to allow the remote to shutdown
+    drop(local);
+    assert_ok!(handle.join());
+}
+
 fn echo_remote(
     connections: usize,
     sync_receiver: Receiver<()>,
@@ -278,6 +489,31 @@ fn echo_remote(
                 };
             }
             assert_eq!(read, written, "unequal reads and writes");
+        }
+    });
+    (handle, assert_ok!(addr_receiver.recv()))
+}
+
+fn _start_remote(
+    connections: usize,
+    sync_receiver: Receiver<()>,
+    barrier: Option<Arc<Barrier>>,
+) -> (thread::JoinHandle<()>, SocketAddr) {
+    let (addr_sender, addr_receiver) = channel();
+    let handle = thread::spawn(move || {
+        let dir = assert_ok!(TempDir::new("uds"));
+        let path = dir.path().join("foo");
+        let remote = assert_ok!(UnixListener::bind(path.clone()));
+        let local_address = assert_ok!(remote.local_addr());
+        assert_ok!(addr_sender.send(local_address));
+
+        for _ in 0..connections {
+            assert_ok!(sync_receiver.recv());
+            let (_local, _) = assert_ok!(remote.accept());
+            if let Some(ref barrier) = barrier {
+                barrier.wait();
+            }
+            drop(_local);
         }
     });
     (handle, assert_ok!(addr_receiver.recv()))
--- tests/util/mod.rs
+++ tests/util/mod.rs
@@ -34,6 +34,26 @@ macro_rules! assert_ok {
     }};
 }
 
+macro_rules! assert_err {
+    ($e:expr) => {
+        assert_err!($e,);
+    };
+    ($e:expr,) => {{
+        use std::result::Result::*;
+        match $e {
+            Ok(v) => panic!("assertion failed: Ok({:?})", v),
+            Err(e) => e,
+        }
+    }};
+    ($e:expr, $($arg:tt)+) => {{
+        use std::result::Result::*;
+        match $e {
+            Ok(v) => panic!("assertion failed: Ok({:?}): {}", v, format_args!($($arg)+)),
+            Err(e) => e,
+        }
+    }};
+}
+
 pub fn init() {
     static INIT: Once = Once::new();
 
