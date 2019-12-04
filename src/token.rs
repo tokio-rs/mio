dac87744817c5c2e48f54eebc8b2fae1798843d1
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-12-04 20:44:58 +0100

    Remove From<usize> for Token

--- src/token.rs
+++ src/token.rs
@@ -130,12 +130,6 @@
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Token(pub usize);
 
-impl From<usize> for Token {
-    fn from(val: usize) -> Token {
-        Token(val)
-    }
-}
-
 impl From<Token> for usize {
     fn from(val: Token) -> usize {
         val.0
