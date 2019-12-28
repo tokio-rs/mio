746be494d85551af743ef9e5aa1263668bdcbd29
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2019-12-28 15:39:23 +0100

    Add shell::IoSourceState

--- src/sys/shell/mod.rs
+++ src/sys/shell/mod.rs
@@ -25,3 +25,24 @@ cfg_uds! {
     mod uds;
     pub(crate) use self::uds::{UnixDatagram, UnixListener, UnixStream};
 }
+
+cfg_net! {
+    use std::io;
+
+    pub struct IoSourceState;
+
+    impl IoSourceState {
+        pub fn new() -> IoSourceState {
+            IoSourceState
+        }
+
+        pub fn do_io<T, F, R>(&self, f: F, io: &T) -> io::Result<R>
+        where
+            F: FnOnce(&T) -> io::Result<R>,
+        {
+            // We don't hold state, so we can just call the function and
+            // return.
+            f(io)
+        }
+    }
+}
