9bbba42f0d7ea8191748bce546bc023a8c4f9113
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2020-03-02 00:08:33 +0000

    Expand on documentation (#1264)
    Expand on documentation
    
    Closes #1223

--- src/lib.rs
+++ src/lib.rs
@@ -36,10 +36,25 @@
 //! ## Guide
 //!
 //! A getting started guide is available in the
-#![cfg_attr(feature = "guide", doc = "[`guide`](../mio/guide/index.html) module.")]
 #![cfg_attr(
-    not(feature = "guide"),
-    doc = "`guide` (only available when the `guide` feature is enabled)."
+    feature = "extra-docs",
+    doc = "[`guide`](../mio/guide/index.html) module."
+)]
+#![cfg_attr(
+    not(feature = "extra-docs"),
+    doc = "`guide` (only available when the `extra-docs` feature is enabled)."
+)]
+//!
+//! ## Available features
+//!
+//! The available features are described in the
+#![cfg_attr(
+    feature = "extra-docs",
+    doc = "[`features`](../mio/features/index.html) module."
+)]
+#![cfg_attr(
+    not(feature = "extra-docs"),
+    doc = "`features` (only available when the `extra-docs` feature is enabled)."
 )]
 
 // macros used internally
@@ -67,21 +82,78 @@ pub use poll::{Poll, Registry};
 pub use token::Token;
 pub use waker::Waker;
 
-#[cfg(unix)]
-cfg_any_os_util! {
-    pub mod unix {
-        //! Unix only extensions.
-        pub use crate::sys::SourceFd;
-    }
+#[cfg(all(unix, feature = "os-util"))]
+#[cfg_attr(docsrs, doc(cfg(all(unix, feature = "os-util"))))]
+pub mod unix {
+    //! Unix only extensions.
+    pub use crate::sys::SourceFd;
 }
 
-// Enable with `cargo doc --features guide`.
-#[cfg(feature = "guide")]
+// Enable with `cargo doc --features extra-docs`.
+#[cfg(feature = "extra-docs")]
+pub mod features {
+    //! # Mio's optional features.
+    //!
+    //! This document describes the available features in Mio.
+    //!
+    #![cfg_attr(feature = "os-poll", doc = "## `os-poll` (enabled)")]
+    #![cfg_attr(not(feature = "os-poll"), doc = "## `os-poll` (disabled)")]
+    //!
+    //! Mio by default provides only a shell implementation, that `panic!`s the
+    //! moment it is actually run. To run it requires OS support, this is
+    //! enabled by activating the `os-poll` feature.
+    //!
+    //! This makes `Poll`, `Registry` and `Waker` functional.
+    //!
+    #![cfg_attr(feature = "os-util", doc = "## `os-util` (enabled)")]
+    #![cfg_attr(not(feature = "os-util"), doc = "## `os-util` (disabled)")]
+    //!
+    //! `os-util` enables additional OS specific facilities. Currently this
+    //! means the `unix` module (with `SourceFd`) becomes available.
+    //!
+    //! ## Network types
+    //!
+    //! Mio provide three features to enable network types:
+    //!
+    #![cfg_attr(feature = "tcp", doc = "* `tcp` (enabled)")]
+    #![cfg_attr(not(feature = "tcp"), doc = "* `tcp` (disabled)")]
+    //! : includes `TcpStream` and `TcpListener`,
+    #![cfg_attr(feature = "udp", doc = "* `udp` (enabled)")]
+    #![cfg_attr(not(feature = "udp"), doc = "* `udp` (disabled)")]
+    //! : includes `UdpSocket`, and
+    #![cfg_attr(feature = "uds", doc = "* `uds` (enabled)")]
+    #![cfg_attr(not(feature = "uds"), doc = "* `uds` (disabled)")]
+    //! : includes `UnixDatagram`, `UnixListener`, `UnixStream` and `SocketAddr`.
+    //!
+    //! All types can be found in the `net` module.
+    //!
+    #![cfg_attr(feature = "extra-docs", doc = "## `extra-docs` (enabled)")]
+    #![cfg_attr(not(feature = "extra-docs"), doc = "## `extra-docs` (disabled)")]
+    //!
+    //! This feature includes additional documentation such as this document and
+    //! the getting started guide. It adds nothing in terms of types (only
+    //! documentation).
+}
+
+// Enable with `cargo doc --features extra-docs`.
+#[cfg(feature = "extra-docs")]
 pub mod guide {
     //! # Getting started guide.
     //!
-    //! Using Mio starts by creating a [`Poll`] instance, which reads events from
-    //! the OS and puts them into [`Events`].
+    //! In this guide we'll do the following:
+    //!
+    //! 1. Create a [`Poll`] instance (and learn what it is).
+    //! 2. Register an [event source].
+    //! 3. Create an event loop.
+    //!
+    //! At the end you'll have a very small (but quick) TCP server that accepts
+    //! connections and then drops (disconnects) them.
+    //!
+    //! ## 1. Creating a `Poll` instance
+    //!
+    //! Using Mio starts by creating a [`Poll`] instance, which monitors events
+    //! from the OS and puts them into [`Events`]. This allows us to execute I/O
+    //! operations based on what operations are ready.
     //!
     //! [`Poll`]: ../struct.Poll.html
     //! [`Events`]: ../event/struct.Events.html
@@ -91,20 +163,34 @@ pub mod guide {
     //! # fn main() -> std::io::Result<()> {
     //! // `Poll` allows for polling of readiness events.
     //! let poll = Poll::new()?;
-    //! // `Events` is collection of readiness `Event`s and can be filled by calling
-    //! // `Poll::poll`.
+    //! // `Events` is collection of readiness `Event`s and can be filled by
+    //! // calling `Poll::poll`.
     //! let events = Events::with_capacity(128);
     //! # drop((poll, events));
     //! # Ok(())
     //! # }
     //! ```
     //!
-    //! Next an [event source] needs to be registered with [`Poll`] to receive
-    //! events for it. We'll use a [`TcpListener`] for which we'll receive an event
-    //! once a connection is ready to be accepted.
+    //! For example if we're using a [`TcpListener`],  we'll only want to
+    //! attempt to accept an incoming connection *iff* any connections are
+    //! queued and ready to be accepted. We don't want to waste our time if no
+    //! connections are ready.
+    //!
+    //! [`TcpListener`]: ../net/struct.TcpListener.html
+    //!
+    //! ## 2. Registering event source
+    //!
+    //! After we've created a [`Poll`] instance that monitors events from the OS
+    //! for us, we need to provide it with a source of events. This is done by
+    //! registering an [event source]. As the name “event source” suggests it is
+    //! a source of events which can be polled using a `Poll` instance. On Unix
+    //! systems this is usually a file descriptor, or a socket/handle on
+    //! Windows.
+    //!
+    //! In the example below we'll use a [`TcpListener`] for which we'll receive
+    //! an event (from [`Poll`]) once a connection is ready to be accepted.
     //!
     //! [event source]: ../event/trait.Source.html
-    //! [`TcpListener`]: ../net/struct.TcpListener.html
     //!
     //! ```
     //! # use mio::net::TcpListener;
@@ -124,12 +210,25 @@ pub mod guide {
     //! # }
     //! ```
     //!
-    //! As the name “event source” suggests it is a source of events which can
-    //! be polled using a `Poll` instance. Multiple event sources can be
-    //! [registered] with `Poll`. After we've done this we can start polling
-    //! these events. If we do this in a loop we've got ourselves an event loop.
+    //! Multiple event sources can be [registered] (concurrently), so we can
+    //! monitor multiple sources at a time.
     //!
-    //! [registered]: ../struct.Registry.html#method.html
+    //! [registered]: ../struct.Registry.html#method.register
+    //!
+    //! ## 3. Creating the event loop
+    //!
+    //! After we've created a [`Poll`] instance and registered one or more
+    //! [event sources] with it, we can [poll] it for events. Polling for events
+    //! is simple, we need a container to store the events: [`Events`] and need
+    //! to do something based on the polled events (this part is up to you, we
+    //! can't do it all!). If we do this in a loop we've got ourselves an event
+    //! loop.
+    //!
+    //! The example below shows the event loop in action, completing our small
+    //! TCP server.
+    //!
+    //! [poll]: ../struct.Poll.html#method.poll
+    //! [event sources]: ../event/trait.Source.html
     //!
     //! ```
     //! # use std::io;
@@ -145,7 +244,7 @@ pub mod guide {
     //! # poll.registry().register(&mut listener, SERVER, Interest::READABLE)?;
     //! // Start our event loop.
     //! loop {
-    //!     // Poll Mio for events, waiting at most 100 milliseconds.
+    //!     // Poll the OS for events, waiting at most 100 milliseconds.
     //!     poll.poll(&mut events, Some(Duration::from_millis(100)))?;
     //!
     //!     // Process each event.
@@ -155,7 +254,7 @@ pub mod guide {
     //!         match event.token() {
     //!             SERVER => loop {
     //!                 // One or more connections are ready, so we'll attempt to
-    //!                 // accept them in a loop.
+    //!                 // accept them (in a loop).
     //!                 match listener.accept() {
     //!                     Ok((connection, address)) => {
     //!                         println!("Got a connection from: {}", address);
