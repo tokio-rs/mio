3cdef54bf0c52c458c1e8a59c0d4ab6d4796f104
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2020-10-14 10:55:23 +0200

    silence clippy warning
    Signed-off-by: Marc-Antoine Perennou <Marc-Antoine@Perennou.com>

--- src/sys/unix/uds/socketaddr.rs
+++ src/sys/unix/uds/socketaddr.rs
@@ -74,6 +74,8 @@ cfg_os_poll! {
         /// Documentation reflected in [`SocketAddr`]
         ///
         /// [`SocketAddr`]: std::os::unix::net::SocketAddr
+        // FIXME: The matches macro requires rust 1.42.0 and we still support 1.39.0
+        #[allow(clippy::match_like_matches_macro)]
         pub fn is_unnamed(&self) -> bool {
             if let AddressKind::Unnamed = self.address() {
                 true
