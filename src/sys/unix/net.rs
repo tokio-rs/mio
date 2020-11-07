8d3ed77d300b07e086b5cd4253dbde4cf5b40780
Linus Färnstrand <faern@faern.net>
2020-11-07 12:06:57 +0100

    More consistent naming

--- src/sys/unix/net.rs
+++ src/sys/unix/net.rs
@@ -84,14 +84,14 @@ impl SocketAddrCRepr {
 /// Converts a Rust `SocketAddr` into the system representation.
 pub(crate) fn socket_addr(addr: &SocketAddr) -> (SocketAddrCRepr, libc::socklen_t) {
     match addr {
-        SocketAddr::V4(ref a) => {
+        SocketAddr::V4(ref addr) => {
             // `s_addr` is stored as BE on all machine and the array is in BE order.
             // So the native endian conversion method is used so that it's never swapped.
-            let sin_addr = libc::in_addr { s_addr: u32::from_ne_bytes(a.ip().octets()) };
+            let sin_addr = libc::in_addr { s_addr: u32::from_ne_bytes(addr.ip().octets()) };
 
             let sockaddr_in = libc::sockaddr_in {
                 sin_family: libc::AF_INET as libc::sa_family_t,
-                sin_port: a.port().to_be(),
+                sin_port: addr.port().to_be(),
                 sin_addr,
                 sin_zero: [0; 8],
                 #[cfg(any(
@@ -108,13 +108,13 @@ pub(crate) fn socket_addr(addr: &SocketAddr) -> (SocketAddrCRepr, libc::socklen_
             let sockaddr = SocketAddrCRepr { v4: sockaddr_in };
             (sockaddr, mem::size_of::<libc::sockaddr_in>() as libc::socklen_t)
         }
-        SocketAddr::V6(ref a) => {
+        SocketAddr::V6(ref addr) => {
             let sockaddr_in6 = libc::sockaddr_in6 {
                 sin6_family: libc::AF_INET6 as libc::sa_family_t,
-                sin6_port: a.port().to_be(),
-                sin6_addr: libc::in6_addr { s6_addr: a.ip().octets() },
-                sin6_flowinfo: a.flowinfo(),
-                sin6_scope_id: a.scope_id(),
+                sin6_port: addr.port().to_be(),
+                sin6_addr: libc::in6_addr { s6_addr: addr.ip().octets() },
+                sin6_flowinfo: addr.flowinfo(),
+                sin6_scope_id: addr.scope_id(),
                 #[cfg(any(
                     target_os = "dragonfly",
                     target_os = "freebsd",
