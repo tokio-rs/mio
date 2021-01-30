6f86b925d3e48f30905d5cfa54348acf3f1fa036
Joseph Shearer <joseph@josephshearer.net>
2021-01-29 19:39:12 -0500

    `accept()`d socket needs to be set to `O_NONBLOCK` on x86 Android
    See https://github.com/tokio-rs/mio/issues/1450

--- src/sys/unix/tcp.rs
+++ src/sys/unix/tcp.rs
@@ -471,7 +471,15 @@ pub fn accept(listener: &net::TcpListener) -> io::Result<(net::TcpStream, Socket
             &mut length
         ))
         .map(|socket| unsafe { net::TcpStream::from_raw_fd(socket) })
-        .and_then(|s| syscall!(fcntl(s.as_raw_fd(), libc::F_SETFD, libc::FD_CLOEXEC)).map(|_| s))
+        .and_then(|s| {
+            syscall!(fcntl(s.as_raw_fd(), libc::F_SETFD, libc::FD_CLOEXEC))?;
+    
+            // See https://github.com/tokio-rs/mio/issues/1450
+            #[cfg(all(target_arch = "x86",target_os = "android"))]
+            syscall!(fcntl(s.as_raw_fd(), libc::F_SETFL, libc::O_NONBLOCK))?;
+            
+            Ok(s)
+        })
     }?;
 
     // This is safe because `accept` calls above ensures the address
