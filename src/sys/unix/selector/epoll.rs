4bacd01a8e1fa1909d5f1c85ebc39fa462b5c022
Tim Zhang <tim@hyper.sh>
2021-05-25 11:40:54 +0800

    Set FD_CLOEXEC for duped epoll_fd
    The close-on-exec flag (FD_CLOEXEC; see fcntl(2)) for the duplicate descriptor is off
    and we should set it manually.
    
    Fixes: tokio-rs/tokio#3809
    
    Signed-off-by: Tim Zhang <tim@hyper.sh>

--- src/sys/unix/selector/epoll.rs
+++ src/sys/unix/selector/epoll.rs
@@ -41,7 +41,7 @@ impl Selector {
     }
 
     pub fn try_clone(&self) -> io::Result<Selector> {
-        syscall!(dup(self.ep)).map(|ep| Selector {
+        syscall!(fcntl(self.ep, libc::F_DUPFD_CLOEXEC)).map(|ep| Selector {
             // It's the same selector, so we use the same id.
             #[cfg(debug_assertions)]
             id: self.id,
