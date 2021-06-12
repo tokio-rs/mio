441367ba70b78989f4de577917f6e8ddfd744063
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2021-06-13 00:33:17 +0200

    Fix Selector::try_clone
    Calls fcntl F_DUPFD_CLOEXEC expects two arguments; the command
    (F_DUPFD_CLOEXEC) and an argument for the command. In this case an lower
    bound for the resulting file descriptor. Because we didn't provide a
    value it would take whatever value was left in the register from
    whatever code used it before the system call.
    
    This caused Waker::new to fail, see issue
    https://github.com/tokio-rs/mio/issues/1497.

--- src/sys/unix/selector/mod.rs
+++ src/sys/unix/selector/mod.rs
@@ -33,3 +33,13 @@ mod kqueue;
     target_os = "openbsd"
 ))]
 pub(crate) use self::kqueue::{event, Event, Events, Selector};
+
+/// Lowest file descriptor used in `Selector::try_clone`.
+///
+/// # Notes
+///
+/// Usually fds 0, 1 and 2 are standard in, out and error. Some application
+/// blindly assume this to be true, which means using any one of those a select
+/// could result in some interesting and unexpected errors. Avoid that by using
+/// an fd that doesn't have a pre-determined usage.
+const LOWEST_FD: libc::c_int = 3;
