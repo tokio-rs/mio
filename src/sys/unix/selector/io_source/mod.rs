fa04dbce9dace23a28d541802d17bbb9b99a96d8
Janrupf <business.janrupf@gmail.com>
2022-10-01 17:35:35 +0200

    Redirect source calls through state on unix

--- src/sys/unix/selector/io_source/mod.rs
+++ src/sys/unix/selector/io_source/mod.rs
@@ -1,1 +1,2 @@
+pub(super) mod edge_triggered;
 pub(super) mod stateless;
--- src/sys/unix/selector/io_source/stateless.rs
+++ src/sys/unix/selector/io_source/stateless.rs
@@ -1,4 +1,6 @@
+use crate::{Interest, Registry, Token};
 use std::io;
+use std::os::unix::io::RawFd;
 
 pub(crate) struct IoSourceState;
 
@@ -8,11 +10,38 @@ impl IoSourceState {
     }
 
     pub fn do_io<T, F, R>(&self, f: F, io: &T) -> io::Result<R>
-        where
-            F: FnOnce(&T) -> io::Result<R>,
+    where
+        F: FnOnce(&T) -> io::Result<R>,
     {
         // We don't hold state, so we can just call the function and
         // return.
         f(io)
+    }
+
+    pub fn register(
+        &mut self,
+        registry: &Registry,
+        token: Token,
+        interests: Interest,
+        fd: RawFd,
+    ) -> io::Result<()> {
+        // Pass through, we don't have any state
+        registry.selector().register(fd, token, interests)
+    }
+
+    pub fn reregister(
+        &mut self,
+        registry: &Registry,
+        token: Token,
+        interests: Interest,
+        fd: RawFd,
+    ) -> io::Result<()> {
+        // Pass through, we don't have any state
+        registry.selector().reregister(fd, token, interests)
+    }
+
+    pub fn deregister(&mut self, registry: &Registry, fd: RawFd) -> io::Result<()> {
+        // Pass through, we don't have any state
+        registry.selector().deregister(fd)
     }
 }
