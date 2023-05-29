7575866fdcd2cdb78865a20cf7dd524fb59e314b
Alice Ryhl <aliceryhl@google.com>
2023-05-29 10:40:06 +0200

    Fix compilation on wasi

--- src/sys/wasi/mod.rs
+++ src/sys/wasi/mod.rs
@@ -79,7 +79,7 @@ impl Selector {
         debug_assert!(events.capacity() >= length);
         #[cfg(debug_assertions)]
         if length == 0 {
-            log::warn!(
+            warn!(
                 "calling mio::Poll::poll with empty subscriptions, this likely not what you want"
             );
         }
