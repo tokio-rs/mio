54e439737f012d969931241c62e52f97f382de8e
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2024-12-03 13:19:07 +0100

    Fix wasm target usage (#1846)

--- examples/tcp_listenfd_server.rs
+++ examples/tcp_listenfd_server.rs
@@ -1,8 +1,8 @@
 // You can run this example from the root of the mio repo:
 // cargo run --example tcp_listenfd_server --features="os-poll net"
 // or with wasi:
-// cargo +nightly build --target wasm32-wasi  --example tcp_listenfd_server --features="os-poll net"
-// wasmtime run --tcplisten 127.0.0.1:9000 --env 'LISTEN_FDS=1' target/wasm32-wasi/debug/examples/tcp_listenfd_server.wasm
+// cargo +nightly build --target wasm32-wasip1  --example tcp_listenfd_server --features="os-poll net"
+// wasmtime run --tcplisten 127.0.0.1:9000 --env 'LISTEN_FDS=1' target/wasm32-wasip1/debug/examples/tcp_listenfd_server.wasm
 
 use mio::event::Event;
 use mio::net::{TcpListener, TcpStream};
