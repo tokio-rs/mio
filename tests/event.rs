a1eb1cd6eb406d966836ef58baefef1d580e8ea2
hulxv <hulxxv@gmail.com>
2025-10-14 16:51:34 +0300

    fix: rustfmt

--- tests/event.rs
+++ tests/event.rs
@@ -1,4 +1,4 @@
-use mio::{Events, event::Event};
+use mio::{event::Event, Events};
 
 // test that Event implements Send and Sync across all platforms
 // this ensures that whenever we add a new platform, we verify Send/Sync
@@ -6,10 +6,10 @@ use mio::{Events, event::Event};
 fn is_event_send_sync() {
     fn assert_send<T: Send>() {}
     fn assert_sync<T: Sync>() {}
-    
+
     assert_send::<Event>();
     assert_sync::<Event>();
-    
+
     assert_send::<Events>();
     assert_sync::<Events>();
 }
