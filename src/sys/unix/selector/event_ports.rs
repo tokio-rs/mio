b90abd6ee82dbe7775b0d9094ad2396458e0234b
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2026-07-30 20:57:12 +0200

    Reuse IoSourceState from poll impl in event_port impl
    The code is exactly the same, no functional changes.

--- src/sys/unix/selector/event_ports.rs
+++ src/sys/unix/selector/event_ports.rs
@@ -721,130 +721,6 @@ pub mod event {
     }
 }
 
-cfg_io_source! {
-    use crate::Registry;
-
-    struct InternalState {
-        selector: Selector,
-        token: Token,
-        interests: Interest,
-        fd: RawFd,
-        shared_record: Arc<RegistrationRecord>,
-    }
-
-    impl Drop for InternalState {
-        fn drop(&mut self) {
-            if self.shared_record.is_registered() {
-                let _ = self.selector.deregister(self.fd);
-            }
-        }
-    }
-
-    pub(crate) struct IoSourceState {
-        inner: Option<Box<InternalState>>,
-    }
-
-    impl IoSourceState {
-        pub fn new() -> IoSourceState {
-            IoSourceState { inner: None }
-        }
-
-        pub fn do_io<T, F, R>(&self, f: F, io: &T) -> io::Result<R>
-        where
-            F: FnOnce(&T) -> io::Result<R>,
-        {
-            let result = f(io);
-
-            if let Err(err) = &result {
-                if err.kind() == io::ErrorKind::WouldBlock {
-                    self.inner.as_ref().map_or(Ok(()), |state| {
-                        state
-                            .selector
-                            .reregister(state.fd, state.token, state.interests)
-                    })?;
-                }
-            }
-
-            result
-        }
-
-        pub fn register(
-            &mut self,
-            registry: &Registry,
-            token: Token,
-            interests: Interest,
-            fd: RawFd,
-        ) -> io::Result<()> {
-            if self.inner.is_some() {
-                Err(io::ErrorKind::AlreadyExists.into())
-            } else {
-                let selector = registry.selector().try_clone()?;
-
-                selector.register_internal(fd, token, interests).map(move |shared_record| {
-                    let state = InternalState {
-                        selector,
-                        token,
-                        interests,
-                        fd,
-                        shared_record,
-                    };
-
-                    self.inner = Some(Box::new(state));
-                })
-            }
-        }
-
-        pub fn reregister(
-            &mut self,
-            registry: &Registry,
-            token: Token,
-            interests: Interest,
-            fd: RawFd,
-        ) -> io::Result<()> {
-            match self.inner.as_mut() {
-                Some(state) => registry.selector().reregister(fd, token, interests).map(|()| {
-                    state.token = token;
-                    state.interests = interests;
-                }),
-                None => Err(io::ErrorKind::NotFound.into()),
-            }
-        }
-
-        pub fn deregister(&mut self, registry: &Registry, fd: RawFd) -> io::Result<()> {
-            if let Some(state) = self.inner.take() {
-                state.shared_record.mark_unregistered();
-            }
-
-            registry.selector().deregister(fd)
-        }
-    }
-}
-
-cfg_io_source! {
-use std::sync::atomic::AtomicBool;
-
-#[derive(Debug)]
-pub(crate) struct RegistrationRecord {
-    is_unregistered: AtomicBool,
-}
-
-impl RegistrationRecord {
-    pub fn new() -> Self {
-        Self {
-            is_unregistered: AtomicBool::new(false),
-        }
-    }
-
-    pub fn mark_unregistered(&self) {
-        self.is_unregistered.store(true, Ordering::Relaxed);
-    }
-
-    pub fn is_registered(&self) -> bool {
-        !self.is_unregistered.load(Ordering::Relaxed)
-    }
-}
-}
-
 impl AsFd for Selector {
     fn as_fd(&self) -> BorrowedFd<'_> {
         self.port.as_fd()
@@ -855,4 +731,10 @@ impl AsRawFd for Selector {
     fn as_raw_fd(&self) -> RawFd {
         self.port.as_raw_fd()
     }
+}
+
+cfg_io_source! {
+    mod registered_io_source;
+    pub(crate) use registered_io_source::IoSourceState;
+    use registered_io_source::RegistrationRecord;
 }
